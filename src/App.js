af0ac30d5712b24604ef67b2f3cf4fdce0c49b40
lingirlsea <lingirlsea@126.com>
2019-11-28 16:54:34 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Axios from "axios"
 import AntdTableVirtulized from './AntdTableVirtualized'
-import { Table } from 'antd'
+// import { Table } from 'antd'
 import columns from './columns'
 import './App.css'
 
